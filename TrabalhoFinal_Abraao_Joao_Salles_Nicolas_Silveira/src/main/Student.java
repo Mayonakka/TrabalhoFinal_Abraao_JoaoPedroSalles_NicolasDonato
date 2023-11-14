61e19f04ca9584075d0336fbb5932e2794300cc3
jsalles1 <joaopedro.lsalles@gmail.com>
2023-11-14 15:26:45 -0300

    Consertei bug em Ver Alunos

--- TrabalhoFinal_Abraao_Joao_Salles_Nicolas_Silveira/src/main/Student.java
+++ TrabalhoFinal_Abraao_Joao_Salles_Nicolas_Silveira/src/main/Student.java
@@ -2,12 +2,12 @@ package main;
 
 import java.util.ArrayList;
 
-public class Student extends Person{
+public class Student extends Person {
 
     private int studentCode;
     private ArrayList<Course> course = new ArrayList();
 
-    public Student(String name, String cpf, int codeStudent) {
+    public Student(String name, String cpf, int studentCode) {
         super(name, cpf);
         this.studentCode = studentCode;
     }
@@ -15,18 +15,21 @@ public class Student extends Person{
     public int getStudentCode() {
         return studentCode;
     }
+
     public ArrayList<Course> getCourse() {
         return course;
     }
+
     public void setStudentCode(int studentCode) {
         this.studentCode = studentCode;
     }
+
     public void setCourse(ArrayList<Course> course) {
         this.course = course;
     }
 
     @Override
     public String toString() {
-        return course + "  |  " + getName();
+        return studentCode + "  |  " + getName();
     }
 }
